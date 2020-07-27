1460a710921919dd99a0cf3a7d3e991876b2438c
Dartegnian <Dartegnian@users.noreply.github.com>
2020-07-28 05:53:26 +0800

    Commit initial backup script

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+cp -r ~/.config/alacritty/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r  ~/.config/bspwm/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/cava/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/ckb-next/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/compton/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/mpd/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/nomacs/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/polybar/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/rofi/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+cp -r ~/.config/sxhkd/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+
+cp ~/.tmux.conf ~/SYGtech/git_gtech/darts-dots/pc/home_pc/tmux
+cp ~/.zshrc ~/SYGtech/git_gtech/darts-dots/pc/home_pc/zsh
