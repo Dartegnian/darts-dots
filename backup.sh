4cfe24d9feafef207cb2608c10ca5a9626d2ab5c
Dartegnian <Dartegnian@users.noreply.github.com>
2020-07-28 06:06:41 +0800

    Use variable instead of hardcoded path

--- backup.sh
+++ backup.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
 
-cp -r ~/.config/alacritty/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r  ~/.config/bspwm/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/cava/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/ckb-next/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/compton/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/mpd/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/nomacs/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/polybar/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/rofi/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
-cp -r ~/.config/sxhkd/ ~/SYGtech/git_gtech/darts-dots/pc/home_pc/config/
+gitDirectory="/home/${USER}/SYGtech/git_gtech/darts-dots"
 
-cp ~/.tmux.conf ~/SYGtech/git_gtech/darts-dots/pc/home_pc/tmux
-cp ~/.zshrc ~/SYGtech/git_gtech/darts-dots/pc/home_pc/zsh
+cp -r ~/.config/alacritty/ ${gitDirectory}/pc/home_pc/config/
+cp -r  ~/.config/bspwm/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/cava/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/ckb-next/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/compton/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/mpd/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/nomacs/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/polybar/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/rofi/ ${gitDirectory}/pc/home_pc/config/
+cp -r ~/.config/sxhkd/ ${gitDirectory}/pc/home_pc/config/
+
+cp ~/.tmux.conf ${gitDirectory}/pc/home_pc/tmux
+cp ~/.zshrc ${gitDirectory}/pc/home_pc/zsh
