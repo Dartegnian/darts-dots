ebf7374d0667fb6d766d27cb534327c836dad439
Dartegnian <root@dartegnian.com>
2020-12-27 15:58:55 +0800

    Make Arch Linux-0 tmux session run first and send to desktop 10

--- pc/home_pc/config/bspwm/tmux.sh
+++ pc/home_pc/config/bspwm/tmux.sh
@@ -3,5 +3,4 @@
 tmux new -d -s delete-me &&
 tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh &&
 tmux kill-session -t delete-me &&
-tmux attach-session -t Singular-1
-
+tmux attach-session -t Arch\ Linux-0
