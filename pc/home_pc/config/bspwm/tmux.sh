809f581ca3f1408a021ee109969f0260d63d5309
Dartegnian <root@dartegnian.com>
2021-01-23 18:37:08 +0800

    Rename initial tmux sesison name

--- pc/home_pc/config/bspwm/tmux.sh
+++ pc/home_pc/config/bspwm/tmux.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-tmux new -d -s delete-me &&
+tmux new -d -s tmux-arch-linux &&
 tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh &&
 tmux kill-session -t delete-me &&
 tmux attach-session -t Arch\ Linux-0
