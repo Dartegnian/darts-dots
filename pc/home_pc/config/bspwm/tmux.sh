a203aed6442df2718b0981a8425b695df8330c97
Dartegnian <root@dartegnian.com>
2021-01-29 21:39:12 +0800

    Forget to rename the session to be deleted

--- pc/home_pc/config/bspwm/tmux.sh
+++ pc/home_pc/config/bspwm/tmux.sh
@@ -2,5 +2,5 @@
 
 tmux new -d -s tmux-arch-linux &&
 tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh &&
-tmux kill-session -t delete-me &&
+tmux kill-session -t tmux-arch-linux &&
 tmux attach-session -t Arch\ Linux-0
