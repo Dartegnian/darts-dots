3bba76650f5fe340e7a86d41dfa39a480e8f728d
Dartegnian <root@dartegnian.com>
2021-05-30 10:03:41 +0800

    Update config after reinstalling for cryptodisk
    Add EOL in zsh, set DPI manually, find fonts in /usr/share folders, update xorg config
    
    
    Add color in pacman, set parallel downloads, add keyfile in mkinitcpio
    
    
    Configure NGINX for SSL
    
    
    Boot from cryptodisk and update GRUB config for crypto
    
    
    Disable bashtop on boot, update tmux window names and paths
    
    
    Update packages list, install r2modman, openresolv, discord-canary

--- pc/arch-main-pc/config/bspwm/tmux.sh
+++ pc/arch-main-pc/config/bspwm/tmux.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 tmux new -d -s tmux-arch-linux &&
-tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh &&
+tmux run-shell ~/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh &&
 tmux kill-session -t tmux-arch-linux &&
-tmux attach-session -t Arch\ Linux-0
+tmux attach-session -t Arch-Linux-0
