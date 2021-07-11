b7c3d8a8c114b2e7c20c728622668b445360ce3a
Dartegnian <root@dartegnian.com>
2021-07-11 23:59:30 +0800

    Only run ckb-next, discordrp-mpris, openrgb once. Separate tmux logic.
    Place other logic in separate file.

---
+++ pc/arch-main-pc/config/bspwm/tmux-startup.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+tmux new -d -s tmux-arch-linux &&
+tmux run-shell ~/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh &&
+tmux kill-session -t tmux-arch-linux &&
---
+++ pc/arch-main-pc/config/bspwm/tmux-terminals.sh
@@ -0,0 +1,7 @@
+cool-retro-term -e tmux attach-session -t Arch-Linux-0 &
+sleep 1
+bspc node -d ^10
+sleep 5
+cool-retro-term -e tmux attach-session -t Singular-1 &
+#sleep 1
+#cool-retro-term -e bashtop &
