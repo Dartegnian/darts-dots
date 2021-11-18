9ae2e7d9355ced1f0482337027dc3a9c2c5be9f0
Dartegnian <root@dartegnian.com>
2021-11-18 14:16:54 +0800

    Start gnome-keyring demon (yes) in bspwm, use alacritty as startup term

--- pc/arch-main-pc/config/bspwm/tmux-terminals.sh
+++ pc/arch-main-pc/config/bspwm/tmux-terminals.sh
@@ -1,7 +1,5 @@
-cool-retro-term -e tmux attach-session -t Arch-Linux-0 &
+alacritty -e tmux attach-session -t Arch-Linux-0 &
 sleep 1
 bspc node -d ^10
 sleep 1
-cool-retro-term -e tmux attach-session -t Singular-1 &
-#sleep 1
-#cool-retro-term -e bashtop &
+alacritty -e tmux attach-session -t Singular-1 &
