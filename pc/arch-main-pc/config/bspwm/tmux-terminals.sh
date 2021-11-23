fda5d050064b161be5b5d5a57b8977d450828e0d
Dartegnian <root@dartegnian.com>
2021-11-24 03:51:22 +0800

    Set terminal color before tmux connection, remove rasi duplicate config
    Properly launch charms bar via sxhkd

--- pc/arch-main-pc/config/bspwm/tmux-terminals.sh
+++ pc/arch-main-pc/config/bspwm/tmux-terminals.sh
@@ -1,5 +1,5 @@
-alacritty -e tmux attach-session -t Arch-Linux-0 &
+alacritty -e zsh -ic "tmux attach-session -t Arch-Linux-0" &
 sleep 1
 bspc node -d ^10
 sleep 1
-alacritty -e tmux attach-session -t Singular-1 &
+alacritty -e zsh -ic "tmux attach-session -t Singular-1" &
